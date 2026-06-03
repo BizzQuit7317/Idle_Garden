3424f5ab3c3b8d0a3a7c80bdf46f092cbb618c56
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2026-06-03 14:27:40 +0100

    Delete src/screens/default.rs

--- src/screens/default.rs
+++
@@ -1,9 +0,0 @@
-use macroquad::prelude::*;
-
-use crate::data;
-
-pub fn draw() -> data::constants::Page  {
-    clear_background(data::constants::DEFAULT_BACKGROUND_COLOR);
-
-    data::constants::Page::Default //Consstantly returning itself unless another page is returned
-}
