4eb58aae44d15b2a6601eb27ec3f173e7b2820ca
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2026-06-06 20:27:32 +0100

    Delete subsystems directory

--- subsystems/bed/overlay.rs
+++
@@ -1,9 +0,0 @@
-use macroquad::prelude::*;
-use macroquad::ui::{root_ui, widgets};
-
-pub fn draw() { 
-    let sw = screen_width();
-    let sh = screen_height();
-    draw_rectangle(sw * 0.1, sh * 0.1, sw * 0.8, sh * 0.8, DARKGRAY);
-    draw_text("Bed System", sw * 0.15, sh * 0.2, 32.0, WHITE);
-}
